d1fa24f596575f8ac04d6ea806ebf40e21ee29b8
Ian <107736504+IanPess@users.noreply.github.com>
2024-02-26 23:51:58 -0300

    Create 8.php

---
+++ lista1/8.php
@@ -0,0 +1,11 @@
+<?php
+$a = floatval(readline());
+$b = floatval(readline());
+$c = floatval(readline());
+$d = floatval(readline());
+
+
+    $e = ($a * $b) - ($c * $d);
+
+    echo "DIFERENCA = ".$e."\n";
+?>
